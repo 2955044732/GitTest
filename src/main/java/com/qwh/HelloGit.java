2985bc1a8d0414aa064132800404b8c75fc21ffd
qwh <qiuwanhong02@163.com>
2023-05-28 23:32:50 +0800

    heihei

---
+++ src/main/java/com/qwh/HelloGit.java
@@ -0,0 +1,12 @@
+package com.qwh;
+
+/**
+ * @auther qwh
+ * @create 2023-05-2023/5/28 23:10
+ */
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("Hello Git");
+        System.out.println("Hello Git1");
+    }
+}
