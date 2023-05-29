22aa311bef5a8915d3c36b43be63b744d66b5ac1
qwh <qiuwanhong02@163.com>
2023-05-29 10:32:12 +0800

    first commit

--- src/main/java/com/qwh/HelloGit.java
+++ src/main/java/com/qwh/HelloGit.java
@@ -10,6 +10,7 @@ public class HelloGit {
         System.out.println("Hello Git1");
         System.out.println("Hello Git2");
         System.out.println("Hello Git2");
+        System.out.println("Hello Git4");
 
     }
 }
